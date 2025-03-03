13ab1bbd0664091e8957a3dafe579ba92af1efd7
ThomasZyway <darkskirime@gmail.com>
2025-03-03 16:00:41 +0100

    Update bdsae.sql

--- projet/mariadb-fp/bdsae.sql
+++ projet/mariadb-fp/bdsae.sql
@@ -1,24 +1,51 @@
--- --------------------------------------------------------
-
--- 
--- Structure de la table 'utilisateur'
--- 
-
 DROP TABLE IF EXISTS utilisateur;
 
 CREATE TABLE utilisateur (
   id int(11) NOT NULL auto_increment,
-  nom varchar(50) default NULL,
-  prenom varchar(50) default NULL,
+  nom varchar(20) default NULL,
+  prenom varchar(20) default NULL,
   email varchar(50) default NULL,
   mot_de_passe varchar(50) default NULL,
   role varchar(50) default NULL,
   PRIMARY KEY  (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+)
 
--- 
--- Contenu de la table 'utilisateur'
--- 
+CREATE TABLE sportif (
+  date_inscription date,
+  niveau_sportif varchar(50)
+)
 
-INSERT INTO utilisateur VALUES (1, 'Dupond', 'Jean', 'test@gmail.com');
+CREATE TABLE coach (
+  tarif_horaire double(11)
+)
+
+CREATE TABLE seance (
+  id int(11) NOT NULL auto_increment,
+  date_heure date,
+  type_seance varchar(50),
+  theme_seance varchar(50),
+  niveau_seance varchar(20),
+  statut varchar(20),
+  PRIMARY KEY  (id),
+  CONSTRAINT `fbcoach` FOREIGN KEY (`coach_id`) REFERENCES `coach` (`id`),
+)
+
+CREATE TABLE fichedepaie (
+  id int(11) NOT NULL auto_increment,
+  periode varchar(50),
+  total_heures int(11),
+  montant_total double(11),
+  PRIMARY KEY  (id),
+  CONSTRAINT `fbcoach` FOREIGN KEY (`coach_id`) REFERENCES `coach` (`id`),
+)
+
+CREATE TABLE seance (
+  id int(11) NOT NULL auto_increment,
+  nom varchar(20),
+  description varchar(50),
+  duree_estimee int(11),
+  difficulte varchar(20),
+  PRIMARY KEY  (id)
+)
+
 
