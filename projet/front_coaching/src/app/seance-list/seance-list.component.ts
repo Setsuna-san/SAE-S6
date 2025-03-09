1130e3ba3d68242bd524db48a79baf3a7e5f70d7
alexis Christaud-braize <126158656+Setsuna-san@users.noreply.github.com>
2025-03-09 16:59:00 +0100

    auth back-end

--- projet/front_coaching/src/app/seance-list/seance-list.component.ts
+++ projet/front_coaching/src/app/seance-list/seance-list.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { Seance } from '../models/seance';
 import { ApiService } from '../services/api.service';
+import { Etatload } from '../models/etatLoad';
 
 @Component({
   selector: 'app-seance-list',
   templateUrl: './seance-list.component.html',
-  styleUrl: './seance-list.component.css'
+  styleUrl: './seance-list.component.css',
 })
 export class SeanceListComponent {
   seances: Seance[] = [];
+  public etatLoad = Etatload.LOADING;
+  readonly etatLoading = Etatload;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getSeances().subscribe((data: Seance[]) => {
-      this.seances = data;
+    this.apiService.getSeances().subscribe({
+      next: (data) => {
+        this.seances = data;
+        this.etatLoad = Etatload.SUCCESS;
+      },
+      error: () => (this.etatLoad = Etatload.ERREUR),
     });
   }
-
-
 }
--- projet/front_coaching/src/app/utilisateur-list/utilisateur-list.component.ts
+++ projet/front_coaching/src/app/utilisateur-list/utilisateur-list.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Utilisateur } from '../models/utilisateur';
 import { ApiService } from '../services/api.service';
+import { Etatload } from '../models/etatLoad';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-utilisateur-list',
   templateUrl: './utilisateur-list.component.html',
-  styleUrl: './utilisateur-list.component.css'
+  styleUrl: './utilisateur-list.component.css',
 })
-export class UtilisateurListComponent {
+export class UtilisateurListComponent implements OnInit{
   utilisateurs: Utilisateur[] = [];
 
-  constructor(private apiService: ApiService) {}
+  public etatLoad = Etatload.LOADING;
+  readonly etatLoading = Etatload;
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.apiService.getUtilisateurs().subscribe((data: Utilisateur[]) => {
-      this.utilisateurs = data;
+    this.apiService.getUtilisateurs().subscribe({
+      next: (data) => {
+        this.utilisateurs = data;
+        this.etatLoad = Etatload.SUCCESS;
+      },
+      error: () => (this.etatLoad = Etatload.ERREUR),
     });
   }
 }
