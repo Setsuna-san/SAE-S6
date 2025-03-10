946985bf4bf7e124033054c4b0bef535ccee6c55
ThomasZyway <darkskirime@gmail.com>
2025-03-10 11:29:41 +0100

    Update SeanceController.php

--- projet/poc_coaching/src/Controller/Api/SeanceController.php
+++ projet/poc_coaching/src/Controller/Api/SeanceController.php
@@ -8,23 +8,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/seances')]
 class SeanceController extends AbstractController
 {
-    #[Route('/', methods: ['GET'])]
+    #[Route('/api/seances', methods: ['GET'])]
     public function index(SeanceRepository $seanceRepository): JsonResponse
     {
         $seances = $seanceRepository->findAll();
         return $this->json($seances, 200, [], ['groups' => 'seance:read']);
     }
 
-    #[Route('/{id}', methods: ['GET'])]
+    #[Route('/api/seance/{id}', methods: ['GET'])]
     public function show(Seance $seance): JsonResponse
     {
         return $this->json($seance, 200, [], ['groups' => 'seance:read']);
     }
 
-    #[Route('/{id}/exercices', methods: ['GET'])]
+    #[Route('/api/seance/{id}/exercices', methods: ['GET'])]
     public function getExercices(Seance $seance): JsonResponse
     {
         $exercices = $seance->getExercices();
