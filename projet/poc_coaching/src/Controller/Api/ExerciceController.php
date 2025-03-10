c5191718fac6ddca98808f569f5f54d012f298be
ThomasZyway <darkskirime@gmail.com>
2025-03-10 10:15:38 +0100

    Update ExerciceController.php

---
+++ projet/poc_coaching/src/Controller/Api/ExerciceController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Exercice;
+use App\Repository\ExerciceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api/exercices')]
+class ExerciceController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    {
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/', methods: ['GET'])]
+    public function index(ExerciceRepository $exerciceRepository): JsonResponse
+    {
+        $exercices = $exerciceRepository->findAll();
+        return $this->json($exercices, 200, [], ['groups' => 'exercice:read']);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(Exercice $exercice): JsonResponse
+    {
+        return $this->json($exercice, 200, [], ['groups' => 'exercice:read']);
+    }
+}
