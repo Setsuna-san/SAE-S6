a23aff98185bc36fa3129636f372060a5a4d3d9a
ThomasZyway <darkskirime@gmail.com>
2025-03-10 14:39:06 +0100

    Update CoachController.php

---
+++ projet/poc_coaching/src/Controller/Api/CoachController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Coach;
+use App\Repository\CoachRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoachController extends AbstractController
+{
+    #[Route('/api/coachs', methods: ['GET'])]
+    public function index(CoachRepository $coachRepository): JsonResponse
+    {
+        $coachs = $coachRepository->findAll();
+        return $this->json($coachs, 200, [], ['groups' => 'coach:read']);
+    }
+
+    #[Route('/api/coach/{id}', methods: ['GET'])]
+    public function show(Coach $coach): JsonResponse
+    {
+        return $this->json($coach, 200, [], ['groups' => 'coach:read']);
+    }
+
+    #[Route('/api/coach/{id}/seances', methods: ['GET'])]
+    public function getExercices(Coach $coach): JsonResponse
+    {
+        $seances = $coach->getSeances();
+        return $this->json($seances, 200, [], ['groups' => 'seance:read']);
+    }
+}
