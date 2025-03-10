c5010fd0816cd0d628f1d738789121ec0ff13deb
ThomasZyway <darkskirime@gmail.com>
2025-03-10 14:39:04 +0100

    Update UtilisateurController.php

--- projet/poc_coaching/src/Controller/Api/UtilisateurController.php
+++ projet/poc_coaching/src/Controller/Api/UtilisateurController.php
@@ -20,31 +20,4 @@ class UtilisateurController extends AbstractController
         return $this->json($utilisateurs, 200, [], ['groups' => 'Utilisateur:read']);
     }
 
-    #[Route('/api/utilisateur/new', methods: ['POST'])]
-    public function create(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher
-    ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-
-        if (!$data || !isset($data['email'], $data['password'], $data['nom'], $data['prenom'])) {
-            return $this->json(['error' => 'Données invalides'], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        $utilisateur = new Utilisateur();
-        $utilisateur->setNom($data['nom']);
-        $utilisateur->setPrenom($data['prenom']);
-        $utilisateur->setEmail($data['email']);
-        $utilisateur->setRoles($data['roles'] ?? ['ROLE_USER']);
-
-        // Hash du mot de passe
-        $hashedPassword = $passwordHasher->hashPassword($utilisateur, $data['password']);
-        $utilisateur->setPassword($hashedPassword);
-
-        $entityManager->persist($utilisateur);
-        $entityManager->flush();
-
-        return $this->json($utilisateur, JsonResponse::HTTP_CREATED, [], ['groups' => 'Utilisateur:read']);
-    }
 }
