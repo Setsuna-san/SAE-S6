d27f608c15cc1974ac8b76d5bd6edff268e90153
alexis Christaud-braize <126158656+Setsuna-san@users.noreply.github.com>
2025-03-12 16:00:14 +0100

    front

--- projet/front_coaching/src/app/models/seance.ts
+++ projet/front_coaching/src/app/models/seance.ts
@@ -1,4 +1,4 @@
-import { Exercice } from "./exercice";
+import { Exercice } from './exercice';
 
 export class Seance {
   constructor(
@@ -6,9 +6,23 @@ export class Seance {
     public coach_id: number = 0,
     public date_heure: Date = new Date(),
     public type_seance: string[] = [],
-    public theme_seance: string = "",
-    public statut: string = "",
-    public niveau_seance: string = "",
+    public theme_seance: string = '',
+    public statut: string = '',
+    public niveau_seance: string = '',
     public exercices: Exercice[] = []
   ) {}
+
+  // Méthode pour formater la date au format "jour mois - heure:minute"
+  formatDate(): string {
+    const jour = this.date_heure.getDate();
+    const moisEnTexte = new Intl.DateTimeFormat('fr-FR', {
+      month: 'long',
+    }).format(this.date_heure);
+    const heure = this.date_heure.getHours();
+    const minutes = this.date_heure.getMinutes();
+
+    return `${jour} ${moisEnTexte} à ${heure}:${
+      minutes < 10 ? '0' + minutes : minutes
+    }`;
+  }
 }
