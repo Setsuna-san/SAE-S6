780dc5922fbd18d2a5267515d0f1c9ed1af3a806
ThomasZyway <darkskirime@gmail.com>
2025-03-13 10:15:30 +0100

    Update SportifController.php

--- projet/poc_coaching/src/Controller/Api/SportifController.php
+++ projet/poc_coaching/src/Controller/Api/SportifController.php
@@ -60,4 +60,30 @@ class SportifController extends AbstractController
 
         return $this->json($sportif, 200, [], ['groups' => 'sportif:read']);
     }
+
+    #[Route('/api/sportif/{id}/seance', methods: ['DELETE'])]
+    public function deleteSeance(SportifRepository $sportifRepository, SeanceRepository $seanceRepository, EntityManagerInterface $entityManager, Request $request, int $id): JsonResponse
+    {
+        $sportif = $sportifRepository->find($id);
+
+        if (!$sportif) {
+            return $this->json(['message' => 'Sportif non trouvé'], 404);
+        }
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return $this->json(['message' => 'Données invalides'], 400);
+        }
+        $seance = $seanceRepository->findOneBy(["id"=> $data]);
+
+        // $seance->addSportif($sportif);
+        // // Ajouter la séance au sportif
+        $sportif->deleteSeance($seance);
+
+
+        $entityManager->persist($seance);
+        $entityManager->flush();
+
+        return $this->json($sportif, 200, [], ['groups' => 'sportif:read']);
+    }
 }
